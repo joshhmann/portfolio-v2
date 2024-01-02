a2984ba3e6bb9b609af707238e7fb752a477aee9
Josh Anderson <joshua.frederick.anderson@gmail.com>
2024-01-01 21:51:56 -0800

    baseline of webpages created

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import './Contact.scss';
+
+function Contact() {
+  return (
+    <motion.div className="Contact">
+      {/* Add your page content here */}
+    </motion.div>
+  );
+}
+
+export default Contact;
---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import './Projects.scss';
+
+function Projects() {
+  return (
+    <motion.div className="Projects">
+      {/* Add your page content here */}
+    </motion.div>
+  );
+}
+
+export default Projects;
